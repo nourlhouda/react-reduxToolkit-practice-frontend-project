ea2b839f1329de117871469346a4672eb011e8d2
Nour Elhouda Jawedi <33379932+nourlhouda@users.noreply.github.com>
2023-07-09 19:49:50 +0100

    adding server url variable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -7,7 +7,7 @@ import store from "./store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-axios.defaults.baseURL="http://localhost:5000"
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
